675df93b88d66f7703dac49e644b17d788c98d81
matthewjaroszek <matthewj369@icloud.com>
2026-04-06 19:41:40 -0400

    done for now

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class SIRS:
 
     def populations(self):
         global x
-        print('') if x else x = False
-        
+        if (x): x = False
+        else: print('')
         print('Populations')
         print(f'Susceptible: {round(self.susceptible, 2)}')
         print(f'Infected: {self.infected}')
