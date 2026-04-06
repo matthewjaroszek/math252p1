c8e13c6c6955cbb17f7bda97272b41a85a51790d
matthewjaroszek <matthewj369@icloud.com>
2026-04-06 19:20:04 -0400

    update

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from config import *
 #SIRS_VD(self, population, infected, transmission, duration, birth, death, immunity = 0)
 
 test_vd = SIRS_VD(1000, 10, 0.3, 10, 0.01, 0.01, 10)
-test = SIRS(1000, 10, 0.3, 10, 10)
-test.summarize()
-test.cycle(1)
-test.summarize()
+test_vd = SIRS(1000, 10, 0.3, 10, 10)
+test_vd.summarize()
+test_vd.cycle(1)
+test_vd.summarize()
